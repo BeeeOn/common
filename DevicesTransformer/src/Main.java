5de55f99949c73bacb8678b074c29976b9cfe076
Robyer <robyer@seznam.cz>
2015-07-13 09:11:22 +0200

    Fix path to specifications directory

--- DevicesTransformer/src/Main.java
+++ DevicesTransformer/src/Main.java
@@ -12,9 +12,11 @@ public class Main {
 
 	public static final String DEFAULT_LANG_CODE = "en";
 
+	public static final String SPECIFICATIONS_DIR = "../specifications/";
+
 	public static void main(String[] args) {
 		// Parsing and generating devices
-		Devices devices = DevicesParser.parseDevices("xml/devices.xml");
+		Devices devices = DevicesParser.parseDevices(SPECIFICATIONS_DIR + "devices.xml");
 		if (devices != null) {
 			DevicesGenerator generator = new DevicesGenerator(devices);
 			generator.generateDevices("export/objects/", "devices.java", new AndroidFormatter());
@@ -25,7 +27,7 @@ public class Main {
 		}
 
 		// Parsing and generating languages
-		List<Language> languages = LanguagesParser.parseLanguages("xml/languages/", "language_", ".xml");
+		List<Language> languages = LanguagesParser.parseLanguages(SPECIFICATIONS_DIR + "languages/", "language_", ".xml");
 		if (!languages.isEmpty()) {
 			LanguagesGenerator generator = new LanguagesGenerator(languages);
 			generator.generateLanguages("export/values/", "export/values-%s/", "generated_strings_devices.xml", DEFAULT_LANG_CODE);
